563a8bd24b803a1e9571736aa48460c4ec5379c4
Ana Amante <ana.amante@overactiveinc.com>
2020-04-29 17:05:43 -0300

    added Chapter6

---
+++ src/ObjectOrientedProgramming6/PhoneBill.java
@@ -0,0 +1,38 @@
+package ObjectOrientedProgramming6;
+
+/**
+ * redo the phone bill calculator to use object-oriented approach.
+ * phone bill should have an ID, a base cost, a number of allotted minutes and a number of minutes used.
+ * It should also be able to calculate the overage, calculate the tax, and calculate the total.
+ * It should also be able to print an itemized bill.
+ * You should also include three constructors
+ * a default one
+ * one that accepts the ID only
+ * one that accepts all fields
+ * Now no matter which of these constructors you use, all fields should be set eventually.
+ * Then you can also create a different class that instantiates the PhoneBill and prints out an itemized bill.
+ */
+
+public class PhoneBill {
+
+    int phoneBillID;
+    double baseCost;
+    int includedMin;
+    int usedMin;
+
+    public calculateOverage(){
+
+    }
+
+    public calculateTax(){
+
+    }
+
+    public calculateTotal(){
+
+    }
+
+    public printBill(){
+
+    }
+}
