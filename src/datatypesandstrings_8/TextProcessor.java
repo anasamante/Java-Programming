6e31cde5a7e0b6b92f7005c0389235acfd59254c
Ana Amante <ana.amante@overactiveinc.com>
2020-05-05 20:11:14 -0300

    added chapt 8  1st excercise

--- src/datatypesandstrings_8/TextProcessor.java
+++ src/datatypesandstrings_8/TextProcessor.java
@@ -1,16 +1,27 @@
 package datatypesandstrings_8;
+
 /*
 create a method that counts the number of words in a String and prints them individually on a new line.
  */
 public class TextProcessor {
 
+    static int numberOfWords;
+
     public static void main(String[] args) {
-        countWords("I love Test Automation University");
+        countTheWords("This is the sentence to count the numbers of words from.");
+    }
+
+    public static void countTheWords(String phrase) {
+        var words = phrase.split(" ");
+        numberOfWords = words.length;
+
+        System.out.println(String.format("Your phrase contains %d words: ", numberOfWords));
+        
+        for (int i = 0; i < numberOfWords; i++) {
+            System.out.println(words[i]);
+        }
 
     }
 
-    public static void countWords(String text){
-        var words = text.split(" ");
 
-    }
 }
